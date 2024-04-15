e33c361d930ad9cce5da49bc9ac1262c8b381a9b
Philip Kjeldtoft Larsen <kjedtoft@gmail.com>
2024-04-15 18:13:04 +0200

    Add query and procedure methods to postgres service

--- src/SkySensorsAPI/InfrastureServices/PostgreSqlInfrastureService.cs
+++ src/SkySensorsAPI/InfrastureServices/PostgreSqlInfrastureService.cs
@@ -1,13 +1,45 @@
-﻿//using Dapper;
+﻿using Npgsql;
+using System.Data;
 
 namespace SkySensorsAPI.InfrastureServices;
 
 public interface IPostgreSqlInfrastureService
 {
-
+	T ExecuteQueryAsync<T>(Func<IDbConnection, T> query);
+	bool ExecuteProcedure(Action<IDbConnection> proc);
 }
 
-public class PostgreSqlInfrastureService : IPostgreSqlInfrastureService
+public class PostgreSqlInfrastureService(
+	IConfiguration configuration,
+	ILogger<PostgreSqlInfrastureService> logger) : IPostgreSqlInfrastureService
 {
 
+	public bool ExecuteProcedure(Action<IDbConnection> proc)
+	{
+		try
+		{
+			using IDbConnection con = new NpgsqlConnection(configuration.GetConnectionString("Postgres"));
+			proc(con);
+			return true;
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "Error executing procedure in database");
+			return false;
+		}
+	}
+
+	public T ExecuteQueryAsync<T>(Func<IDbConnection, T> query)
+	{
+		try
+		{
+			using IDbConnection con = new NpgsqlConnection(configuration.GetConnectionString("Postgres"));
+			return query(con);
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "Error executing query in database");
+			return default;
+		}
+	}
 }
