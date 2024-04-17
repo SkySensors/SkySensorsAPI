a40799ca014c9ac0c3bf994e7820ff106824b078
Philip Kjeldtoft Larsen <kjedtoft@gmail.com>
2024-04-17 15:09:25 +0200

    Formatting

--- src/SkySensorsAPI/Models/MeasuredSensorValuesDTO.cs
+++ src/SkySensorsAPI/Models/MeasuredSensorValuesDTO.cs
@@ -4,12 +4,12 @@ using System.Text.Json.Serialization;
 
 namespace SkySensorsAPI.Models
 {
-    public class MeasuredSensorValuesDTO
-    {
+	public class MeasuredSensorValuesDTO
+	{
 		[JsonConverter(typeof(JsonStringEnumConverter))] // Convert the Enum to string 
 		public SensorType Type { get; set; }
 		[JsonConverter(typeof(PhysicalAddressConverter))] // Needed to convert PhysicalAddress to string when used in endpoint result
 		public PhysicalAddress MacAddress { get; set; }
-        public SensorValueDTO[] SensorValues { get; set; }
-    }
+		public SensorValueDTO[] SensorValues { get; set; }
+	}
 }
