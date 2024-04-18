07021c0d988f8577a05fa99dce0ec413e63d72ad
Jonas Hansen <jonas.hansen@visma.com>
2024-04-18 15:39:15 +0200

    Fix tests

--- src/SkySensorsAPI.Tests/IntegrationTests/Controllers/WeatherStationControllerTests.cs
+++ src/SkySensorsAPI.Tests/IntegrationTests/Controllers/WeatherStationControllerTests.cs
@@ -5,14 +5,14 @@ namespace SkySensorsAPI.Tests.IntegrationTests.Controllers;
 
 internal class WeatherStationControllerTests : IntegrationTests
 {
-	private const string UrlPath = "/weatherstation";
+	private const string UrlPath = "/api/weatherstation";
 
 	[Test]
 	public async Task GET_WeatherStation_ResponseWith200()
 	{
 		// Arrange
 		// Act
-		HttpResponseMessage response = await HttpClient.GetAsync(UrlPath);
+		HttpResponseMessage response = await HttpClient.GetAsync(UrlPath + "?macAddress=90-A2-DA-10-55-88&startTime=1713355703952&endTime=1713442103952");
 
 		// Assert
 		response.StatusCode.Should().Be(HttpStatusCode.OK);
