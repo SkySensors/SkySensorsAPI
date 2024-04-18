aadc4eb8f2fa1fe5270b59e8d69b8aa8c938588b
Jonas Hansen <jonas.hansen@visma.com>
2024-04-18 15:49:29 +0200

    Test

--- src/SkySensorsAPI.Tests/IntegrationTests/Controllers/WeatherStationControllerTests.cs
+++ src/SkySensorsAPI.Tests/IntegrationTests/Controllers/WeatherStationControllerTests.cs
@@ -15,6 +15,8 @@ internal class WeatherStationControllerTests : IntegrationTests
 		HttpResponseMessage response = await HttpClient.GetAsync(UrlPath + "?macAddress=90-A2-DA-10-55-88&startTime=1713355703952&endTime=1713442103952");
 
 		// Assert
+		var a = await response.Content.ReadAsStringAsync();
+		a.Should().Be("a");
 		response.StatusCode.Should().Be(HttpStatusCode.OK);
 	}
 }
