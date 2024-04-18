8483d8f05cbb436cec0d941fa0ce5569fe0b6afc
Jonas Hansen <jonas.hansen@visma.com>
2024-04-18 15:34:12 +0200

    Added test sample

---
+++ src/SkySensorsAPI.Tests/IntegrationTests/Controllers/WeatherStationControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System.Net;
+
+namespace SkySensorsAPI.Tests.IntegrationTests.Controllers;
+
+internal class WeatherStationControllerTests : IntegrationTests
+{
+	private const string UrlPath = "/weatherstation";
+
+	[Test]
+	public async Task GET_WeatherStation_ResponseWith200()
+	{
+		// Arrange
+		// Act
+		HttpResponseMessage response = await HttpClient.GetAsync(UrlPath);
+
+		// Assert
+		response.StatusCode.Should().Be(HttpStatusCode.OK);
+	}
+}
