df65102c505c34691cf713dc77fd14296bc1f516
Jonas Hansen <jonas.hansen@visma.com>
2024-04-19 09:52:11 +0200

    Fixed post endpoints

--- src/SkySensorsAPI/Models/Dto/MeasuredSensorValuesDTO.cs
+++ src/SkySensorsAPI/Models/Dto/MeasuredSensorValuesDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.NetworkInformation;
+﻿using SkySensorsAPI.Utilities;
+using System.Net.NetworkInformation;
 using System.Text.Json.Serialization;
 
 namespace SkySensorsAPI.Models.DTO;
@@ -7,6 +8,7 @@ public class MeasuredSensorValuesDTO
 {
 	[JsonConverter(typeof(JsonStringEnumConverter))] // Convert the Enum to string 
 	public SensorType Type { get; set; }
+	[JsonConverter(typeof(PhysicalAddressConverter))] // Needed to convert PhysicalAddress to string when used in endpoint result
 	public required PhysicalAddress MacAddress { get; set; }
 	public List<SensorValueDTO> SensorValues { get; set; } = [];
 }
