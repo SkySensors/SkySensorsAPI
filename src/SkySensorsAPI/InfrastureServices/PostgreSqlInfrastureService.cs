b2e9aa7abffd61912eb02ae09ddaf15a19aacde4
Philip <kjedtoft@gmail.com>
2024-04-23 17:03:17 +0200

    First commit to test api use own db and environment variables

--- src/SkySensorsAPI/InfrastureServices/PostgreSqlInfrastureService.cs
+++ src/SkySensorsAPI/InfrastureServices/PostgreSqlInfrastureService.cs
@@ -14,11 +14,17 @@ public interface IPostgreSqlInfrastureService
 
 public class PostgreSqlInfrastureService(IConfiguration configuration) : IPostgreSqlInfrastureService
 {
+	private string connectionStr = "Username=" + Environment.GetEnvironmentVariable("DB_USERNAME") + 
+		";Password=" + Environment.GetEnvironmentVariable("DB_PASS") + 
+		";Host=" + Environment.GetEnvironmentVariable("DB_HOST") + 
+		";Port=" + Environment.GetEnvironmentVariable("DB_PORT") + 
+		";Database=" + Environment.GetEnvironmentVariable("DB_NAME") + 
+		";Pooling=true;Connection Lifetime=0;Application Name=SkySensorAPI";
 	public async Task<T> ExecuteQueryAsync<T>(Func<IDbConnection, Task<T>> query)
 	{
 		DefaultTypeMap.MatchNamesWithUnderscores = true;
 		SqlMapper.AddTypeHandler(new NpgsqlTypeHandler<PhysicalAddress>(NpgsqlDbType.MacAddr));
-		using IDbConnection con = new NpgsqlConnection(configuration.GetConnectionString("Postgres"));
+		using IDbConnection con = new NpgsqlConnection(connectionStr);
 		return await query(con);
 	}
 }
