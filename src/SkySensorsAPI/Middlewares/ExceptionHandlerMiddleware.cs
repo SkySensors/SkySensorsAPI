98f3e53003e1d119100cbb07912e9cc7a9ba5434
Philip Kjeldtoft Larsen <kjedtoft@gmail.com>
2024-04-23 13:10:52 +0200

    Add search for duplicate key value violates unique constraint message

--- src/SkySensorsAPI/Middlewares/ExceptionHandlerMiddleware.cs
+++ src/SkySensorsAPI/Middlewares/ExceptionHandlerMiddleware.cs
@@ -15,9 +15,9 @@ public class ExceptionHandlerMiddleware(RequestDelegate nextMiddleware)
 			ex is FormatException ||
 			ex is JsonException ||
 			ex is ArgumentException ||
-			ex is PostgresException)
+			(ex is PostgresException && ex.Message.StartsWith("23505"))) //23505 is duplicate key value violates unique constraint
 		{
-			context.Response.StatusCode = 400;
+            context.Response.StatusCode = 400;
 			context.Response.ContentType = "text/plain";
 			await context.Response.WriteAsync(ex.Message);
 		}
